35a521630f9c1a9aa9cba36fe2797a341f2a3b24
Zhenboro <83726285+Zhenboro@users.noreply.github.com>
2025-12-07 11:44:59 -0300

    Instancia model helper text

--- BorocitoCMD/urls.py
+++ BorocitoCMD/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Borocito-CS Panel'
+admin.site.site_title = 'Borocito-CS'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls", namespace="api")),
--- api/models.py
+++ api/models.py
@@ -5,17 +5,17 @@ import uuid
 # Create your models here.
 
 class Instancia(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    key = models.CharField(max_length=70, null=True, blank=True) # api key used to create the instance. REVIEW
-    borocito = models.CharField(max_length=70, null=True, blank=True)
-    username = models.CharField(max_length=70, null=True, blank=True)
-    domain = models.CharField(max_length=70, null=True, blank=True)
-    local_ip = models.GenericIPAddressField(null=True, blank=True)
-    public_ip = models.GenericIPAddressField(null=True, blank=True)
-    language = models.CharField(max_length=70, null=True, blank=True)
-    windows = models.CharField(max_length=70, null=True, blank=True)
-    ram = models.CharField(max_length=70, null=True, blank=True)
-    screen = models.CharField(max_length=90, null=True, blank=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="UUID", help_text="Unique identificator for the instance.")
+    key = models.CharField(max_length=70, null=True, blank=True, verbose_name="Key-Pair", help_text="Key-Pair used to make requests.")
+    borocito = models.CharField(max_length=70, null=True, blank=True, verbose_name="Borocito", help_text="Borocito instance information.")
+    username = models.CharField(max_length=70, null=True, blank=True, verbose_name="User name", help_text="Windows user name account.")
+    domain = models.CharField(max_length=70, null=True, blank=True, verbose_name="Domain", help_text="Windows domain name.")
+    local_ip = models.GenericIPAddressField(null=True, blank=True, verbose_name="Local IP", help_text="Local network IP.")
+    public_ip = models.GenericIPAddressField(null=True, blank=True, verbose_name="Public IP", help_text="Public network IP.")
+    language = models.CharField(max_length=70, null=True, blank=True, verbose_name="Language", help_text="Windows language.")
+    windows = models.CharField(max_length=70, null=True, blank=True, verbose_name="Windows", help_text="Windows information.")
+    ram = models.CharField(max_length=70, null=True, blank=True, verbose_name="RAM", help_text="RAM available.")
+    screen = models.CharField(max_length=90, null=True, blank=True, verbose_name="Screen", help_text="Screen information.")
     
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True, auto_now_add=False)
